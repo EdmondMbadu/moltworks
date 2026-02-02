b5e58ffc00f096be0f3762c8b8ea5a42b5e37a27
Edmond Mbadu <mbadungoma@gmail.com>
2026-02-01 17:32:13 -0800

    added routing support

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,22 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'jobs/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'jobs/:id/claim',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'jobs/:id/submit',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'jobs/:id/review',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
